e0a748ec431e110047485d43504e23940d050e57
farwaMuhibzada <farwafarid2017@gmail.com>
2024-06-24 12:47:16 +0430

    fixed the eslintrc errors .

--- script.js
+++ script.js
@@ -14,30 +14,29 @@ const numerals = [
   ['IX', 9],
   ['V', 5],
   ['IV', 4],
-  ['I', 1]
+  ['I', 1],
 ];
-input.addEventListener('keydown', e => {
-  if (e.key == 'Enter') {
+input.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter') {
     convert.click();
   }
-  });
-  convert.addEventListener('click', () => { 
+});
+convert.addEventListener('click', () => { 
   let value = input.value;
-  if (!value){
+  if (!value) {
     output.innerText = 'Please enter a valid number';
   } else if (value < 0) {
-      output.innerText = 'Please enter a number greater than or equal to 1';
+    output.innerText = 'Please enter a number greater than or equal to 1';
   } else if (value > 3999) {
-      output.innerText = 'Please enter a number less than or equal to 3999';
+    output.innerText = 'Please enter a number less than or equal to 3999';
   } else {
-      let result = '';
-      for (const[roman, number] of numerals) {
-         while(value >= number) {
-        result += roman;
-        value -= number;
+    let result = '';
+    for (const [roman, number] of numerals) {
+         while (value >= number) {
+          result += roman;
+          value -= number;
       }
   }
-     output.innerText = result; 
-    }
+    output.innerText = result; 
+  }
 });
-    
