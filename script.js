ddf27aee19338321210faddeef34e381568dadff
farwaMuhibzada <farwafarid2017@gmail.com>
2024-06-24 12:09:46 +0430

    added an array of arraye for roman numbers .

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 const input = document.getElementById('number');
 const convert = document.getElementById('convert-btn');
 const output = document.getElementById('output');
+const numerals = [
+    ['M', 1000],
+    ['CM', 900],
+    ['D', 500],
+    ['CD', 400],
+    ['C', 100],
+    ['XC', 90],
+    ['L', 50],
+    ['XL', 40],
+    ['X', 10],
+    ['IX', 9],
+    ['V', 5],
+    ['IV', 4],
+    ['I', 1]
+  ];
